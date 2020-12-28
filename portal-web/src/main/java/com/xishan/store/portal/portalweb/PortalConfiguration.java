80f805a8b4456777e587dcd57791d34c21b77324
liuxishan <1776358671@qq.com>
2020-12-28 08:42:51 +0800

    修改configuration

--- portal-web/src/main/java/com/xishan/store/portal/portalweb/PortalConfiguration.java
+++ portal-web/src/main/java/com/xishan/store/portal/portalweb/PortalConfiguration.java
@@ -1,10 +1,7 @@
 package com.xishan.store.portal.portalweb;
 
-import com.xishan.store.usercenter.userweb.UserWebAutoStarterAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({UserWebAutoStarterAutoConfiguration.class})//装配
 public class PortalConfiguration {
 }
